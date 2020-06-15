15ce5cf5eb52176527de5daf9b178bc3610c76ed
Jaswantsinghh <jaswantsinghjsn@gmail.com>
2020-06-15 18:01:44 +0530

    correction in sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,6 @@ self.addEventListener('install', evt => {
   console.log('service worker has been installed');
 });
 //activate service Worker
-self.addEventListener('activate', evt -> {
+self.addEventListener('activate', evt => {
   console.log('service worker has been activated');
 });
