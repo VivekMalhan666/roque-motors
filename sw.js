cf48ac65f913c4447243b0f56ef0fbcd9eb04c8a
Jaswantsinghh <jaswantsinghjsn@gmail.com>
2020-06-15 17:48:21 +0530

    service worker activated

--- sw.js
+++ sw.js
@@ -2,3 +2,7 @@
 self.addEventListener('install', evt => {
   console.log('service worker has been installed');
 });
+//activate service Worker
+self.addEventListener('activate', evt -> {
+  console.log('service worker has been activated');
+});
